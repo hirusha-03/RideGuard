d50c96e67dbc61032bd4f43544e85ceefefad9a5
Thiruni Wijerathne <thiruwijerathne@gmail.com>
2025-10-05 00:32:46 +0530

    Changes done Readme and UI

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pandas as pd
 import streamlit as st
 from datetime import time
 
+# Load model
+model = joblib.load("saved_models/D_T_model_pipeline.pkl")
+
+
 # CSS for full-width layout and mobile responsiveness
 st.markdown("""
     <style>
@@ -160,9 +164,6 @@ with col_result1:
     predict_button = st.button("🚦 Predict Ride Cancellation")
 
 with col_result2:
-
-    # Load model
-    model = joblib.load("saved_models/D_T_model_pipeline.pkl")
 
     # Prediction result displayed to the right of button
     if predict_button:
